5ab843c36dacae606c8186bed6eaf2ab84c62673
Alex Pogue <acpogue1226@gmail.com>
2022-06-13 22:41:57 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import App from './App';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
@@ -7,11 +7,10 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<React.StrictMode>
+  <App />
+</React.StrictMode>);
 
 
