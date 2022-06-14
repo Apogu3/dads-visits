a3e21f69a3b24cf719abc58ae93d804e4becb693
Alex Pogue <acpogue1226@gmail.com>
2022-06-14 13:54:18 -0500

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = ({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: 'AIzaSyBShuaCk1_jTVGQWbmnOaZlcLxb2cIgknQ',
+  authDomain: 'chat-appv1-38f7e.firebaseapp.com',
+  databaseURL: 'http://chat-appv1-38f7e.firebaseio.com',
+  projectId: 'chat-appv1-38f7e',
+  storageBucket: 'chat-appv1-38f7e.appspot.com',
+  messagingSenderId: '449788042576',
+  appId: '1:449788042576:web:f9bd2a7abd5a037515e055',
 })
 
 // Initialize Firebase
