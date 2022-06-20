5f5763c66d6accf7e39cbb2e8555e9fa3ff09ae8
Alex Pogue <acpogue1226@gmail.com>
2022-06-20 17:13:48 -0500

    commit

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -6,8 +6,8 @@ import Data from '../data/ag-offices.json';
 
  function Resources() {
   const [viewport, setViewport] = useState({
-    width: "100",
-    height: "100",
+    width: "60vw",
+    height: "60vh",
     zoom: 9.5
   });
   
